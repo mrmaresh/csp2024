cad2e20cab31ffa234c2ee4680448d1c394757dc
Jason Maresh <85368513+mrmaresh@users.noreply.github.com>
2024-12-09 10:04:40 -0800

    l

--- python/vocab.py
+++ python/vocab.py
@@ -15,8 +15,8 @@ def main():
     # List to store incorrect words
     incorrect_words = []
 
-    run_quiz(vocab, words, definitions)
-    end_quiz()
+    run_quiz(vocab, words, definitions, incorrect_words)
+    end_quiz(incorrect_words)
 
 
 def create_vocab(vocab):
@@ -30,8 +30,9 @@ def create_vocab(vocab):
 
 
 
-def run_quiz(vocab, words, definitions):
+def run_quiz(vocab, words, definitions, incorrect_words):
     system('clear')
+    random.shuffle(definitions)
     for definition in definitions:
         print(f"Definition: {definition} \n")
 
@@ -41,10 +42,44 @@ def run_quiz(vocab, words, definitions):
                 correct_word = word
                 break
 
+        # create a list of options
+        options = [correct_word]
+        while len(options) < 4:
+            random_word = random.choice(words)
+            if random_word not in options:
+                options.append(random_word)
+        random.shuffle(options) # Shuffle to randomize the order
+
+        # Present options
+        number = 1
+        for option in options:
+            print(f"{number}. {option}")
+            number = number + 1
+
+        # Get user input and check the answer
+        answer = input("Your choice (1-4): ")
+        print()
+
+        if options[int(answer) - 1] != correct_word:
+            print(f"Incorrect! The correct word was {correct_word}.")
+            incorrect_words.append(correct_word)
+
+        print()
+        get_string("Press enter to continue ...")
+        system('clear')
 
 
-def end_quiz():
-    pass
+
+
+
+
+def end_quiz(incorrect_words):
+    if len(incorrect_words) != 0:
+        print("You got the following words wrong:\n")
+        for word in incorrect_words:
+            print(word)
+    else:
+        print("Congratulations! You answered all questions correctly.")
 
 
 
